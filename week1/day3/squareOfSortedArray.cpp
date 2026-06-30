578591dee0cf4f11967a40e7d8a86418eb450b4f
Mansi Agarwal <mansiagarwal.9700@gmail.com>
2026-06-30 17:38:45 +0530

    day 3 soltion 2

---
+++ week1/day3/squareOfSortedArray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> sortedSquares(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> result(n);
+
+    int left = 0, right = n - 1;
+    int pos = n - 1;
+
+    while (left <= right) {
+        int leftSq = nums[left] * nums[left];
+        int rightSq = nums[right] * nums[right];
+
+        if (leftSq > rightSq) {
+            result[pos] = leftSq;
+            left++;
+        } else {
+            result[pos] = rightSq;
+            right--;
+        }
+        pos--;
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> nums = {-4, -1, 0, 3, 10};
+
+    vector<int> ans = sortedSquares(nums);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
